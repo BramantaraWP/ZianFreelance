aca6216b2369af796f5e62b50dbc9ec146440640
BramantaraWP <ikomangbramantara@gmail.com>
2025-05-31 23:26:52 +0800

    Update status.js

--- status.js
+++ status.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 3000;
+export default function handler(req, res) {
+  function getRandomValue(min, max, unit = '') {
+    const value = Math.floor(Math.random() * (max - min + 1)) + min;
+    return `${value}${unit}`;
+  }
 
-app.use(cors());
-
-// Function buat random nilai
-function getRandomValue(min, max, unit = '') {
-  const value = Math.floor(Math.random() * (max - min + 1)) + min;
-  return `${value}${unit}`;
-}
-
-// API route
-app.get('/api/status', (req, res) => {
   const data = {
-    bandwidth: getRandomValue(50, 300, " Mbps"),
-    ping: getRandomValue(2, 40, "ms"),
-    ram: `${getRandomValue(100, 1024)}Mb/1Tb`,
+    bandwidth: getRandomValue(20, 100, " Mbps"),
+    ping: getRandomValue(5, 40, "ms"),
+    ram: `${getRandomValue(200, 1000)}Mb/1Tb`,
     cpu: `${getRandomValue(1, 1000)}%`
   };
 
-  res.json(data);
-});
-
-app.listen(PORT, () => {
-  console.log(`API Server running on http://localhost:${PORT}`);
-});
+  res.status(200).json(data);
+}
