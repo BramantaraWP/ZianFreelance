dd3baba7c84a09af939d5eb1ea843e78fede5749
BramantaraWP <ikomangbramantara@gmail.com>
2025-05-31 23:40:13 +0800

    Update status.js

--- status.js
+++ status.js
@@ -1,14 +1,9 @@
 export default function handler(req, res) {
-  function getRandomValue(min, max, unit = '') {
-    const value = Math.floor(Math.random() * (max - min + 1)) + min;
-    return `${value}${unit}`;
-  }
-
   const data = {
-    bandwidth: getRandomValue(20, 100, " Mbps"),
-    ping: getRandomValue(5, 40, "ms"),
-    ram: `${getRandomValue(200, 1000)}Mb/1Tb`,
-    cpu: `${getRandomValue(1, 1000)}%`
+    bandwidth: `${(Math.random() * 100).toFixed(2)} Mbps`,
+    ping: `${Math.floor(Math.random() * 300)} ms`,
+    ram: `${(Math.random() * 16).toFixed(2)} GB`,
+    cpu: `${(Math.random() * 100).toFixed(1)} %`
   };
 
   res.status(200).json(data);
